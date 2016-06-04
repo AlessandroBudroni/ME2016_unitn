ac4377d2b708d1e7bc9cf6a6cb38e4ec6de340c8
Alessandro Budroni <alessandro.budroni@miracl.com>
2016-06-04 23:36:29 +0100

    now it works, I've also fixed some bugs 2

--- Text_Retrieval/README.md
+++ Text_Retrieval/README.md
@@ -1,17 +1,13 @@
-
 SIMPLE TEXT RETRIEVAL PYTHON
 
-This is a simple python script to search images of Chalearn cultural events dataset.
+This is a simple python script to search related images of other images saved in a directory /dataset trough google image.
 
 To make it run from bash go in the Text Retrieval folder and run: python text_extraction.py
 
-You have to set in text_extraction.py the main folder of your dataset at line 16.
+Te formats supported are:
 
-It is not multi-thread so on big datasets you either implement it or use the trick of opening multiple bash and call it multiple times selecting the folder you want: text_extraction.py at lines 17-19.
-
-If needed call format_text.py to delete spaces, puntuation  etc after you completed the dataset. 
-
-NOTE: I developed it quickly and I noticed sometimes it gets struck so keep an eye on it while is working.
-
-Andrea Pilzer
-andrea.pilzer@studenti.unitn.it
+* gif
+* tif
+* png
+* jpg
+* jpeg
--- Text_Retrieval/text_extraction.py
+++ Text_Retrieval/text_extraction.py
@@ -41,16 +41,20 @@ for currEvent in events:
   if currEvent == 'Non-Class':
     images = [images[-1]]
   for currImage in images:
-    print('--- FOR IMAGE')
-    links = text_retrieval.find_related_images(mainPath+currEvent+'/'+currImage) 
-
-    numArticle = 1
-    os.mkdir(mainPath+currEvent+'/'+currImage[0:6])
-    linkFile = open(mainPath+currEvent+'/'+currImage[0:6]+'/'+'links.txt','w')
+    if not ((".jpeg" in currImage) or (".jpg" in currImage) or (".png" in currImage) or (".gif" in currImage) or (".svg" in currImage)):
+        continue
+    print('--- FOR IMAGE %s' % currImage)
+    links = text_retrieval.find_related_images(mainPath+currEvent+'/'+currImage)
+    currImageName = file_navigator.extract_name(currImage)
+    if not os.path.exists(mainPath+currEvent+'/'+currImageName):
+      os.mkdir(mainPath+currEvent+'/'+currImageName)
+    linkFile = open(mainPath+currEvent+'/'+currImageName+'/'+'links.txt','w')
     for url in links:
-      linkFile.write('%s ' % url)
+      linkFile.write('%s\n' % url)
     linkFile.close()
 
+''' I'm commenting this for the moment because I didn't get really what it does and it's slow
+    numArticle = 1
     for url in links:
       print('FOR LOOP')
       try:
@@ -76,5 +80,4 @@ for currEvent in events:
         numArticle += 1
       except:
         print('Bad Link')
-        numArticle += 1
-
+        numArticle += 1     '''
